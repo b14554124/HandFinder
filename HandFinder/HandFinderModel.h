51d928f63e1e354227ebd167c38bcab9b5a04247
BCZ <505763451@qq.com>
2019-07-24 10:57:51 +0800

    ...
    ..

---
+++ HandFinder/HandFinderModel.h
@@ -0,0 +1,24 @@
+//
+//  HandFinderModel.h
+//  Horoscope
+//
+//  Created by BCZ on 2018/11/27.
+//  Copyright © 2018 SM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HandFinderModel : NSObject
+
+
+@property (nonatomic, assign) float left;
+@property (nonatomic, assign) float Back;
+@property (nonatomic, assign) float Front;
+@property (nonatomic, assign) float Hand;
+@property (nonatomic, assign) float Right;
+
+@end
+
+NS_ASSUME_NONNULL_END
